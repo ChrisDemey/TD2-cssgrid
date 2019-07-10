a8c9e202edeb6a8a1aa1310c93b5ee66ef3a0807
Chris <demey.christophe.dc@gmail.com>
2019-07-10 16:31:00 +0200

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,10 @@
-// navs (marques, exo, gearsets)
-function brandClick() {
-    let brandtab = document.querySelector(".brandlist");
-    if (brandtab.style.display === "block") {
-        brandtab.style.display = "none";
+// nav
+function tabClick(className) {
+    let tab = document.querySelector(className);
+    if (tab.style.display === "block") {
+        tab.style.display = "none";
     } 
     else{
-        brandtab.style.display = "block";
-    }
-
-    let exotab = document.querySelector(".exolist");
-    if (exotab.style.display === "block") {
-        exotab.style.display = "none";
-    }
-
-    let geartab = document.querySelector(".gearlist");
-    if (geartab.style.display === "block") {
-        geartab.style.display = "none";
+        tab.style.display = "block";
     }
 }
-
-function exoClick() {
-    let exotab = document.querySelector(".exolist");
-    if (exotab.style.display === "block") {
-        exotab.style.display = "none";
-    }
-    else {
-        exotab.style.display = "block";
-    }
-
-    let brandtab = document.querySelector(".brandlist");
-    if (brandtab.style.display === "block") {
-        brandtab.style.display = "none";
-    } 
-
-    let geartab = document.querySelector(".gearlist");
-    if (geartab.style.display === "block") {
-        geartab.style.display = "none";
-    }
-}
-
-function gearClick() {
-    let geartab = document.querySelector(".gearlist");
-    if (geartab.style.display === "block") {
-        geartab.style.display = "none";
-    }
-    else {
-        geartab.style.display = "block";
-    }
-
-    let brandtab = document.querySelector(".brandlist");
-    if (brandtab.style.display === "block") {
-        brandtab.style.display = "none";
-    } 
-
-    let exotab = document.querySelector(".exolist");
-    if (exotab.style.display === "block") {
-        exotab.style.display = "none";
-    }
-}
-
-// tableaux marques
